4f2cf4fd842a714b0bde2470190cfdf9f9638b57
Tolaseeq <toljaserg7@mail.ru>
2022-05-25 16:27:02 +0300

    Сделал выбор директории, а не файла, запретил экспорт в существующий файл

---
+++ src/main/java/ru/mai/activetest/Entry.java
@@ -0,0 +1,9 @@
+package ru.mai.activetest;
+
+import ru.mai.activetest.MainApplication;
+
+public class Entry {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
